5ca34a6c398ef52ec3937cb66d3d4ce0f78f210f
Aaron <aaronrod@gmail.com>
2017-07-05 16:28:57 +0100

    Adds test that indexes the page from content

--- spec/indexer_spec.rb
+++ spec/indexer_spec.rb
@@ -13,6 +13,11 @@ describe PageIndexer do
     expect(indexer.page_indexed).to be_empty
   end
 
+  it "indexes pages content by counting frequency of words" do
+    content = "and the raven raven never flitting still is sitting still is never sitting"
+    expect(indexer.indexing(content)).to eq({"flitting"=>1, "raven"=>2, "sitting"=>2})
+  end
+
   xit "excludes stop words from content" do
     content = "and the raven raven never flitting still is sitting still is never sitting"
     expect(indexer.exclude_stopwords(content)).to eq(["raven", "raven", "flitting", "sitting", "sitting"])
