a30835bed1ec2133950e62aa40a15039c60c024e
georn <mrog0@outlook.com>
2017-07-06 14:02:59 +0100

    setting the right formating for the csv file

--- lib/seed_data.rb
+++ lib/seed_data.rb
@@ -14,9 +14,25 @@ class SeedData
 	end
 
 	def store_in_csv(csv_file = 'seeddata.csv')
-		csv = CSV.open(csv_file, 'a+', col_sep: "|") do |row|
+		csv = CSV.open(csv_file, 'a+', col_sep: "|", quote_char: "|") do |row|
 			row << [@seed, @urls, @keywords, @description, @headers, @text]
+			p @urls
+
 		end
 	end
+
+	# def store_in_csv(csv_file = 'seeddata.csv')
+	# 	string_csv = CSV.generate do |csv|
+	# 		csv << [@seed, @urls, @keywords, @description, @headers, @text]
+	# 	end
+	# end
+
+	# parsed_csv = CSV.parse(File.read('./seeddata.csv'), headers: true)
+	# p parsed_csv.class
+	# p parsed_csv
+	# parsed_csv.each do |row|
+	# 	p row.class
+	# 	p row['urls']
+	# end
 
 end
